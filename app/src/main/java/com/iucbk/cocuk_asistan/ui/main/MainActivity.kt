ba9af75a220865d89f4c8d24a574996d1a682662
Miraç Özkan <mirac.ozkan123@gmail.com>
2020-03-02 13:21:27 +0300

    🧱 Projenin temeli atıldı (#7)

---
+++ app/src/main/java/com/iucbk/cocuk_asistan/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.iucbk.cocuk_asistan.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.iucbk.cocuk_asistan.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
