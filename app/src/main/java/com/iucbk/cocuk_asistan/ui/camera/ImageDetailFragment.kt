73e493f2c73736eb71c71397eadc49431074f135
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-03-28 23:12:06 +0300

    implement ui with event to image detail fragment
    Signed-off-by: Mirac Ozkan <mirac.ozkan123@gmail.com>

--- app/src/main/java/com/iucbk/cocuk_asistan/ui/camera/ImageDetailFragment.kt
+++ app/src/main/java/com/iucbk/cocuk_asistan/ui/camera/ImageDetailFragment.kt
@@ -1,8 +1,15 @@
 package com.iucbk.cocuk_asistan.ui.camera
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.iucbk.cocuk_asistan.R
 import com.iucbk.cocuk_asistan.common.BaseFragment
 import com.iucbk.cocuk_asistan.databinding.FragmentImageDetailBinding
@@ -22,10 +29,52 @@ class ImageDetailFragment : BaseFragment<ImageDetailViewModel>(R.layout.fragment
 
     private val binding by viewBinding(FragmentImageDetailBinding::bind)
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun initUI() {
+        super.initUI()
+        val activity = (requireActivity() as AppCompatActivity)
+        activity.setSupportActionBar(binding.bottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                val action = ImageDetailFragmentDirections.actionImageDetailFragmentToHomeFragment()
+                findNavController().navigate(action)
+                true
+            }
+            R.id.knowledge -> {
+                findNavController().popBackStack()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun initUserActionObservers() {
+        super.initUserActionObservers()
+        binding.fltShootPhotoAgain.setOnClickListener {
+            val action =
+                ImageDetailFragmentDirections.actionImageDetailFragmentToCameraFragment()
+            findNavController().navigate(action)
+        }
+    }
+
     override fun initObservers() {
         super.initObservers()
         mainViewModel.imageFile.observe(viewLifecycleOwner, Observer {
-            binding.text.text = it.absoluteFile.absolutePath
+            binding.imgShootedImage.setImageURI(it.toUri())
         })
     }
 }
