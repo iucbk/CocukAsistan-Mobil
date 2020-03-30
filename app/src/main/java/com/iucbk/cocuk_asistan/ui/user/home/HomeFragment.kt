d4c8d99f297cb8a33e072e3ea716f00f895d9559
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-03-30 19:56:21 +0300

    added double back press for exit to the app over the home screen
    Signed-off-by: Mirac Ozkan <mirac.ozkan123@gmail.com>

--- app/src/main/java/com/iucbk/cocuk_asistan/ui/user/home/HomeFragment.kt
+++ app/src/main/java/com/iucbk/cocuk_asistan/ui/user/home/HomeFragment.kt
@@ -3,6 +3,7 @@ package com.iucbk.cocuk_asistan.ui.user.home
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.iucbk.cocuk_asistan.R
@@ -11,6 +12,7 @@ import com.iucbk.cocuk_asistan.util.constant.CAMERA_IMAGE_ID
 import com.iucbk.cocuk_asistan.util.constant.UPLOAD_IMAGE_PERMISSION_CAMERA
 import com.iucbk.cocuk_asistan.util.extension.checkPermissions
 import com.iucbk.cocuk_asistan.util.extension.requestPermission
+import com.iucbk.cocuk_asistan.util.extension.showToast
 import com.iucbk.cocuk_asistan.util.extension.viewBinding
 
 /**
@@ -20,9 +22,27 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private val binding by viewBinding(FragmentHomeBinding::bind)
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        handleBackPress()
         initUserActions()
+    }
+
+    private fun handleBackPress() {
+        var backPressCounter = 0
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (backPressCounter != 0) {
+                        requireActivity().finish()
+                    } else {
+                        showToast(getString(R.string.tap_again))
+                        backPressCounter++
+                    }
+                }
+            })
     }
 
     private fun initUserActions() {
