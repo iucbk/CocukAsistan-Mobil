eeab0db0aca91f45585d4864af40a6dd9672958e
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-04-04 00:48:11 +0300

    update login response and save user data to sp
    Signed-off-by: Mirac Ozkan <mirac.ozkan123@gmail.com>

--- app/src/main/java/com/iucbk/cocuk_asistan/ui/user/home/HomeFragment.kt
+++ app/src/main/java/com/iucbk/cocuk_asistan/ui/user/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.iucbk.cocuk_asistan.ui.user.home
 
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
@@ -8,10 +9,13 @@ import com.iucbk.cocuk_asistan.R
 import com.iucbk.cocuk_asistan.common.BaseFragment
 import com.iucbk.cocuk_asistan.databinding.FragmentHomeBinding
 import com.iucbk.cocuk_asistan.enums.Permissions
+import com.iucbk.cocuk_asistan.util.constant.USER_FULL_NAME
 import com.iucbk.cocuk_asistan.util.extension.checkPermissions
 import com.iucbk.cocuk_asistan.util.extension.requestPermission
 import com.iucbk.cocuk_asistan.util.extension.showToast
 import com.iucbk.cocuk_asistan.util.extension.viewBinding
+import com.iucbk.cocuk_asistan.util.getData
+import javax.inject.Inject
 
 /**
  * A simple [Fragment] subclass.
@@ -22,11 +26,18 @@ class HomeFragment : BaseFragment<HomeViewModel>(R.layout.fragment_home) {
         return HomeViewModel::class.java
     }
 
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
     private val binding by viewBinding(FragmentHomeBinding::bind)
+    private val userName by lazy {
+        (sharedPreferences.getData(USER_FULL_NAME, "") as String).substringBefore(" ")
+    }
 
     override fun initUI() {
         super.initUI()
         handleBackPress()
+        binding.include2.txtGreeting.text = String.format(getString(R.string.greeting), userName)
     }
 
     private fun handleBackPress() {
--- app/src/main/java/com/iucbk/cocuk_asistan/util/CredentialUtils.kt
+++ app/src/main/java/com/iucbk/cocuk_asistan/util/CredentialUtils.kt
@@ -1,7 +1,10 @@
 package com.iucbk.cocuk_asistan.util
 
 import android.content.SharedPreferences
+import android.util.Log
 import com.iucbk.cocuk_asistan.data.net.response.login.LoginResponse
+import com.iucbk.cocuk_asistan.util.constant.USER_EMAIL
+import com.iucbk.cocuk_asistan.util.constant.USER_FULL_NAME
 import com.iucbk.cocuk_asistan.util.constant.USER_TOKEN
 
 
@@ -18,5 +21,15 @@ fun SharedPreferences.getToken() =
     this.getData(USER_TOKEN, "") as String
 
 
-fun SharedPreferences.saveSession(loginResponse: LoginResponse?) =
-    putData(USER_TOKEN, loginResponse?.token)
+fun SharedPreferences.saveSession(loginResponse: LoginResponse?): Boolean {
+    return try {
+        putData(USER_TOKEN, loginResponse?.token)
+        putData(USER_EMAIL, loginResponse?.email)
+        putData(USER_FULL_NAME, loginResponse?.fullName)
+        true
+    } catch (e: Exception) {
+        Log.e("Saving Session : ", e.localizedMessage ?: "Saving Session")
+        false
+    }
+}
+
